06aaee3e7043039c12ce1703bd4390383189d7a3
Lexi <nblexi98@gmail.com>
2026-08-06 19:20:09 -0500

    added search by icao and local_code, added better documentation

--- airport.py
+++ airport.py
@@ -1,9 +1,12 @@
 def get_airport_id(airports: dict, airport_ident: str) -> str:
     for airport_id, airport in airports.items():
-        if airport.ident == airport_ident:
+        if (len(airport_ident) == 3 and airport.iata == airport_ident) or (len(airport_ident) == 4 and airport.ident == airport_ident) or (len(airport_ident) == 4 and airport.icao == airport_ident) or (len(airport_ident) == 3 and airport.local_code == airport_ident) or (len(airport_ident) == 4 and airport.local_code == airport_ident):
             return airport_id
+
+    return "356332" # returning small airfield in lima which should never happen
 
 def get_airport_id_from_internal(airports: dict, internal_id: int) -> str:
     for airport_id, airport in airports.items():
         if airport.internal_id == internal_id:
             return airport_id
+    return "40073" # again ^
--- readme.md
+++ readme.md
@@ -1,3 +1,27 @@
 # Aviation Tool
 
 Find airport weather and distances to other airports within a time range
+
+#### Download the latest release or build it yourself:
+
+## Clone [aviation-tool](https://github.com/rainbowardite/aviation-tool.git)
+
+## Get Data
+Get `airports.csv`, `countries.csv`, and `runways.csv` from [ourairports.com](https://ourairports.com/data/) and place them in the `/files` folder
+
+## Run:
+### Terminal
+`python ./aviation-tool.py`
+
+### exe [Windows Only (I think?)]
+
+#### Build exe
+
+##### Terminal
+`python -m PyInstaller .\aviation-tool.spec`
+
+##### Batch File
+run batch file at `/update_executable.bat`
+
+#### Run exe
+exe outputs to `dist/aviation-tool.exe`
